608a16cd807af6beeb4be1cfd8cff0bdf2cc2680
Tomy20050211 <tomasruiz1104@gmail.com>
2026-01-16 15:56:24 -0500

    pixels

--- start.js
+++ start.js
@@ -1,21 +1,24 @@
 const playButton = document.getElementById("playButton");
 const containerPlay = document.getElementById("containerPlayConformation");
+const overlay = document.getElementById("overlay");
+const bgMusic = document.getElementById("bgMusic");
 
 playButton.addEventListener("click", () => {
-    containerPlay.classList.add("active");
-    bgMusic.volume = 0.9; // opcional
+  containerPlay.classList.add("active");
+  overlay.classList.add("active");
+  bgMusic.volume = 0.9;
   bgMusic.play();
 });
 
-containerPlay.addEventListener("click", (e) => {
-    if (e.target === containerPlay) {
-        containerPlay.classList.remove("active");
-    }
-});
+const closeModal = () => {
+  containerPlay.classList.remove("active");
+  overlay.classList.remove("active");
+};
+
+overlay.addEventListener("click", closeModal);
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-        containerPlay.classList.remove("active");
-    }
+  if (e.key === "Escape") {
+    closeModal();
+  }
 });
-
